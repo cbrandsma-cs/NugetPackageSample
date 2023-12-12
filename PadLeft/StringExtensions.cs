d5c64813bd58b12b2feb8f4aeb80f819c0bdda6c
Chris Brandsma <cbrandsma@cspace.com>
2023-12-12 08:36:38 -0700

    first commit

---
+++ PadLeft/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PadLeft
+{
+    public static class StringExtensions
+    {
+        public static string MyPadLeft(this string text, int count)
+        {
+            if (text.Length < count)
+            {
+                return text.PadLeft(count);
+            }
+
+            return text;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # NugetPackageSample
+
+Playing with creating my own private Nuget package
+
+## Commands
+
+1. `dotnet pack --configuration Release`
+2. `dotnet nuget push /PadLeft/bin/Release/PadLeft.1.0.0.nupkg --source "github"`
